c947b79a7b55ef57ab04ae5babe151f284a747e7
Logan Gore <lgore@stytch.com>
2024-11-21 08:28:24 -0500

    Define Public Token stubs and schematization

--- internal/provider/resources/publictoken.go
+++ internal/provider/resources/publictoken.go
@@ -6,34 +6,31 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/stytchauth/stytch-management-go/pkg/api"
 )
 
 // Ensure the implementation satisfies the expected interfaces.
 var (
-	_ resource.Resource              = &secretResource{}
-	_ resource.ResourceWithConfigure = &secretResource{}
+	_ resource.Resource              = &publicTokenResource{}
+	_ resource.ResourceWithConfigure = &publicTokenResource{}
 )
 
-func NewSecretResource() resource.Resource {
-	return &secretResource{}
+func NewPublicTokenResource() resource.Resource {
+	return &publicTokenResource{}
 }
 
-type secretResource struct {
+type publicTokenResource struct {
 	client *api.API
 }
 
-type secretModel struct {
-	SecretID  types.String `tfsdk:"secret_id"`
-	ProjectID types.String `tfsdk:"project_id"`
-	CreatedAt types.String `tfsdk:"created_at"`
-	Secret    types.String `tfsdk:"secret"`
+type publicTokenModel struct {
+	PublicToken types.String `tfsdk:"public_token"`
+	ProjectID   types.String `tfsdk:"project_id"`
+	CreatedAt   types.String `tfsdk:"created_at"`
 }
 
-func (r *secretResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+func (r *publicTokenResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
 	// Add a nil check when handling ProviderData because Terraform
 	// sets that data after it calls the ConfigureProvider RPC.
 	if req.ProviderData == nil {
@@ -55,48 +52,40 @@ func (r *secretResource) Configure(ctx context.Context, req resource.ConfigureRe
 }
 
 // Metadata returns the resource type name.
-func (r *secretResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-	resp.TypeName = req.ProviderTypeName + "_secret"
+func (r *publicTokenResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_public_token"
 }
 
 // Schema defines the schema for the resource.
-func (r *secretResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+func (r *publicTokenResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
-			"secret_id": schema.StringAttribute{
+			"public_token": schema.StringAttribute{
 				Computed:    true,
-				Description: "The unique identifier for the secret.",
+				Description: "The public token value. This is a unique ID which is also the identifier for the token.",
 			},
 			"project_id": schema.StringAttribute{
 				Required:    true,
 				Description: "The unique identifier for the project.",
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.RequiresReplace(),
-				},
 			},
 			"created_at": schema.StringAttribute{
 				Computed:    true,
-				Description: "The ISO-8601 timestamp when the secret was created.",
-			},
-			"secret": schema.StringAttribute{
-				Computed:    true,
-				Sensitive:   true,
-				Description: "The secret value.",
+				Description: "The ISO-8601 timestamp when the public token was created.",
 			},
 		},
 	}
 }
 
 // Create creates the resource and sets the initial Terraform state.
-func (r *secretResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	var plan secretModel
+func (r *publicTokenResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var plan publicTokenModel
 	diags := req.Plan.Get(ctx, &plan)
 	resp.Diagnostics.Append(diags...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// TODO: Generate API request body from plan and call r.client.Secrets.Create
+	// TODO: Generate API request body from plan and call r.client.PublicTokens.Create
 
 	diags = resp.State.Set(ctx, plan)
 	resp.Diagnostics.Append(diags...)
@@ -106,9 +95,9 @@ func (r *secretResource) Create(ctx context.Context, req resource.CreateRequest,
 }
 
 // Read refreshes the Terraform state with the latest data.
-func (r *secretResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+func (r *publicTokenResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
 	// Get the current state
-	var state secretModel
+	var state publicTokenModel
 	diags := req.State.Get(ctx, &state)
 	resp.Diagnostics.Append(diags...)
 	if resp.Diagnostics.HasError() {
@@ -126,18 +115,18 @@ func (r *secretResource) Read(ctx context.Context, req resource.ReadRequest, res
 }
 
 // Update updates the resource and sets the updated Terraform state on success.
-func (r *secretResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+func (r *publicTokenResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
 	resp.Diagnostics.AddError("Update not allowed", "Updating this resource is not supported. Please delete and recreate the resource.")
 }
 
 // Delete deletes the resource and removes the Terraform state on success.
-func (r *secretResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	var state secretModel
+func (r *publicTokenResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var state publicTokenModel
 	diags := req.State.Get(ctx, &state)
 	resp.Diagnostics.Append(diags...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// TODO: Call r.client.Secrets.Delete
+	// TODO: Call r.client.PublicTokens.Delete
 }
