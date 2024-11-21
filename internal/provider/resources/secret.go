40216f26b731b12e599df027913c6944b853e911
Logan Gore <lgore@stytch.com>
2024-11-21 10:49:07 -0500

    Use RequiresReplace

--- internal/provider/resources/secret.go
+++ internal/provider/resources/secret.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/stytchauth/stytch-management-go/pkg/api"
 )
@@ -68,6 +70,9 @@ func (r *secretResource) Schema(_ context.Context, _ resource.SchemaRequest, res
 			"project_id": schema.StringAttribute{
 				Required:    true,
 				Description: "The unique identifier for the live project.",
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.RequiresReplace(),
+				},
 			},
 			"created_at": schema.StringAttribute{
 				Computed:    true,
