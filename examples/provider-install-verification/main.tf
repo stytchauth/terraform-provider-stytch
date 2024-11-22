bef3b4f1ba0f154c8b467484f9370c0bc4347aa3
Logan Gore <lgore@stytch.com>
2024-11-22 16:20:52 -0500

    Implementation for redirectURL resource

--- examples/provider-install-verification/main.tf
+++ examples/provider-install-verification/main.tf
@@ -17,3 +17,45 @@ resource "stytch_project" "b2b_project" {
   name     = "tf-b2b"
   vertical = "B2B"
 }
+
+resource "stytch_redirect_url" "consumer_redirect_url" {
+  project_id = stytch_project.consumer_project.test_project_id
+  url        = "http://localhost:3000/consumer"
+  valid_types = [
+    {
+      type       = "LOGIN"
+      is_default = true
+    },
+    {
+      type       = "SIGNUP"
+      is_default = true
+    },
+    {
+      type       = "INVITE"
+      is_default = false
+    }
+  ]
+}
+
+resource "stytch_redirect_url" "b2b_redirect_url" {
+  project_id = stytch_project.b2b_project.test_project_id
+  url        = "http://localhost:3000/b2b"
+  valid_types = [
+    {
+      type       = "LOGIN"
+      is_default = true
+    },
+    {
+      type       = "SIGNUP"
+      is_default = true
+    },
+    {
+      type       = "INVITE"
+      is_default = false
+    },
+    {
+      type       = "DISCOVERY"
+      is_default = true
+    }
+  ]
+}
