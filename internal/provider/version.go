b9e9462d0126fd15acb086be17b8bc091aaa14ce
Logan Gore <lgore@stytch.com>
2024-12-03 16:45:55 -0500

    Fix auto-publish workflow... again

--- internal/provider/version.go
+++ internal/provider/version.go
@@ -1,3 +1,6 @@
 package provider
 
+// Version is the current version of the stytch terraform provider.
+// A GitHub action detects changes in this file in order to trigger
+// new releases to be tagged and built.
 const Version = "0.0.6"
