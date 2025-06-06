8610807ebe224e461a7d3c01160b09726da3a014
Bruno Peynetti @ Stytch <bpeynetti@stytch.com>
2025-06-06 17:39:07 -0400

    Add support for user locking (#69)

--- examples/resources/stytch_project/resource.tf
+++ examples/resources/stytch_project/resource.tf
@@ -27,3 +27,29 @@ resource "stytch_project" "b2b_project_cross_org" {
   test_cross_org_passwords_enabled = true
   live_cross_org_passwords_enabled = false
 }
+
+# Create a consumer project with user lock self-serve enabled
+# and custom lock thresholds
+resource "stytch_project" "consumer_project_lock_self_serve" {
+  name     = "tf-consumer-lock-self-serve"
+  vertical = "CONSUMER"
+
+  test_user_lock_self_serve_enabled = true
+  live_user_lock_self_serve_enabled = true
+
+  test_user_lock_threshold = 20
+  live_user_lock_threshold = 5
+}
+
+# Create a B2B project with user lock self-serve enabled
+# and custom lock TTLs
+resource "stytch_project" "b2b_project_lock_self_serve" {
+  name     = "tf-b2b-lock-self-serve"
+  vertical = "B2B"
+
+  test_user_lock_self_serve_enabled = true
+  live_user_lock_self_serve_enabled = true
+
+  test_user_lock_ttl = 300  # 5 minutes
+  live_user_lock_ttl = 7200 # 2 hours
+}
--- internal/provider/resources/project_test.go
+++ internal/provider/resources/project_test.go
@@ -19,29 +19,57 @@ var (
 type projectResourceArgs struct {
 	name                  string
 	vertical              projects.Vertical
-	liveImpersonation     bool
-	testImpersonation     bool
-	liveCrossOrgPasswords bool
-	testCrossOrgPasswords bool
+	liveImpersonation     *bool
+	testImpersonation     *bool
+	liveCrossOrgPasswords *bool
+	testCrossOrgPasswords *bool
+	liveUserLockSelfServe *bool
+	testUserLockSelfServe *bool
+	liveUserLockThreshold *int32
+	testUserLockThreshold *int32
+	liveUserLockTTL       *int32
+	testUserLockTTL       *int32
 }
 
 func projectResource(args projectResourceArgs) string {
-	return fmt.Sprintf(`
+	config := fmt.Sprintf(`
 resource "stytch_project" "test" {
-  name                             = "%s"
-  vertical                         = "%s"
-  live_user_impersonation_enabled  = %t
-  test_user_impersonation_enabled  = %t
-  live_cross_org_passwords_enabled = %t
-  test_cross_org_passwords_enabled = %t
-}
-`, args.name,
-		string(args.vertical),
-		args.liveImpersonation,
-		args.testImpersonation,
-		args.liveCrossOrgPasswords,
-		args.testCrossOrgPasswords,
-	)
+  name     = "%s"
+  vertical = "%s"`, args.name, string(args.vertical))
+
+	if args.liveImpersonation != nil {
+		config += fmt.Sprintf("\n  live_user_impersonation_enabled = %t", *args.liveImpersonation)
+	}
+	if args.testImpersonation != nil {
+		config += fmt.Sprintf("\n  test_user_impersonation_enabled = %t", *args.testImpersonation)
+	}
+	if args.liveCrossOrgPasswords != nil {
+		config += fmt.Sprintf("\n  live_cross_org_passwords_enabled = %t", *args.liveCrossOrgPasswords)
+	}
+	if args.testCrossOrgPasswords != nil {
+		config += fmt.Sprintf("\n  test_cross_org_passwords_enabled = %t", *args.testCrossOrgPasswords)
+	}
+	if args.liveUserLockSelfServe != nil {
+		config += fmt.Sprintf("\n  live_user_lock_self_serve_enabled = %t", *args.liveUserLockSelfServe)
+	}
+	if args.testUserLockSelfServe != nil {
+		config += fmt.Sprintf("\n  test_user_lock_self_serve_enabled = %t", *args.testUserLockSelfServe)
+	}
+	if args.liveUserLockThreshold != nil {
+		config += fmt.Sprintf("\n  live_user_lock_threshold = %d", *args.liveUserLockThreshold)
+	}
+	if args.testUserLockThreshold != nil {
+		config += fmt.Sprintf("\n  test_user_lock_threshold = %d", *args.testUserLockThreshold)
+	}
+	if args.liveUserLockTTL != nil {
+		config += fmt.Sprintf("\n  live_user_lock_ttl = %d", *args.liveUserLockTTL)
+	}
+	if args.testUserLockTTL != nil {
+		config += fmt.Sprintf("\n  test_user_lock_ttl = %d", *args.testUserLockTTL)
+	}
+
+	config += "\n}"
+	return config
 }
 
 func TestAccProjectResource(t *testing.T) {
@@ -92,6 +120,8 @@ func TestAccProjectResource(t *testing.T) {
 			})
 		})
 	}
+
+	// Test user impersonation settings
 	for _, tc := range []struct {
 		liveImpersonationBefore bool
 		testImpersonationBefore bool
@@ -112,8 +142,8 @@ func TestAccProjectResource(t *testing.T) {
 						Config: testutil.ProviderConfig + projectResource(projectResourceArgs{
 							name:              "test",
 							vertical:          projects.VerticalConsumer,
-							liveImpersonation: tc.liveImpersonationBefore,
-							testImpersonation: tc.testImpersonationBefore,
+							liveImpersonation: &tc.liveImpersonationBefore,
+							testImpersonation: &tc.testImpersonationBefore,
 						}),
 						Check: resource.ComposeAggregateTestCheckFunc(
 							resource.TestCheckResourceAttr("stytch_project.test", "name", "test"),
@@ -135,8 +165,8 @@ func TestAccProjectResource(t *testing.T) {
 						Config: testutil.ProviderConfig + projectResource(projectResourceArgs{
 							name:              "test2",
 							vertical:          projects.VerticalConsumer,
-							liveImpersonation: tc.liveImpersonationAfter,
-							testImpersonation: tc.testImpersonationAfter,
+							liveImpersonation: &tc.liveImpersonationAfter,
+							testImpersonation: &tc.testImpersonationAfter,
 						}),
 						Check: resource.ComposeAggregateTestCheckFunc(
 							resource.TestCheckResourceAttr("stytch_project.test", "name", "test2"),
@@ -150,6 +180,7 @@ func TestAccProjectResource(t *testing.T) {
 		})
 	}
 
+	// Test cross org passwords settings
 	for _, tc := range []struct {
 		liveCrossOrgPasswordsBefore bool
 		testCrossOrgPasswordsBefore bool
@@ -170,8 +201,8 @@ func TestAccProjectResource(t *testing.T) {
 						Config: testutil.ProviderConfig + projectResource(projectResourceArgs{
 							name:                  "test",
 							vertical:              projects.VerticalB2B,
-							liveCrossOrgPasswords: tc.liveCrossOrgPasswordsBefore,
-							testCrossOrgPasswords: tc.testCrossOrgPasswordsBefore,
+							liveCrossOrgPasswords: &tc.liveCrossOrgPasswordsBefore,
+							testCrossOrgPasswords: &tc.testCrossOrgPasswordsBefore,
 						}),
 						Check: resource.ComposeAggregateTestCheckFunc(
 							resource.TestCheckResourceAttr("stytch_project.test", "name", "test"),
@@ -193,13 +224,155 @@ func TestAccProjectResource(t *testing.T) {
 						Config: testutil.ProviderConfig + projectResource(projectResourceArgs{
 							name:                  "test2",
 							vertical:              projects.VerticalB2B,
-							liveCrossOrgPasswords: tc.liveCrossOrgPasswordsAfter,
-							testCrossOrgPasswords: tc.testCrossOrgPasswordsAfter,
+							liveCrossOrgPasswords: &tc.liveCrossOrgPasswordsAfter,
+							testCrossOrgPasswords: &tc.testCrossOrgPasswordsAfter,
 						}),
 						Check: resource.ComposeAggregateTestCheckFunc(
 							resource.TestCheckResourceAttr("stytch_project.test", "name", "test2"),
 							resource.TestCheckResourceAttr("stytch_project.test", "live_cross_org_passwords_enabled", strconv.FormatBool(tc.liveCrossOrgPasswordsAfter)),
 							resource.TestCheckResourceAttr("stytch_project.test", "test_cross_org_passwords_enabled", strconv.FormatBool(tc.testCrossOrgPasswordsAfter)),
+						),
+					},
+					// Delete testing automatically occurs in resource.TestCase
+				},
+			})
+		})
+	}
+
+	// Test user lock self-serve settings
+	for _, tc := range []struct {
+		liveUserLockSelfServeBefore bool
+		testUserLockSelfServeBefore bool
+		liveUserLockSelfServeAfter  bool
+		testUserLockSelfServeAfter  bool
+		liveUserLockThresholdBefore int32
+		testUserLockThresholdBefore int32
+		liveUserLockThresholdAfter  int32
+		testUserLockThresholdAfter  int32
+	}{
+		{true, true, false, false, 1, 2, 1, 2},
+		{true, false, false, true, 1, 2, 1, 2},
+		{false, true, true, true, 1, 2, 1, 2},
+		// false, false is already tested above
+	} {
+		t.Run(fmt.Sprintf("user lock self-serve %t-%t", tc.liveUserLockSelfServeBefore, tc.testUserLockSelfServeBefore), func(t *testing.T) {
+			resource.Test(t, resource.TestCase{
+				ProtoV6ProviderFactories: testutil.TestAccProtoV6ProviderFactories,
+				Steps: []resource.TestStep{
+					{
+						// Create and Read testing
+						Config: testutil.ProviderConfig + projectResource(projectResourceArgs{
+							name:                  "test",
+							vertical:              projects.VerticalConsumer,
+							liveUserLockSelfServe: &tc.liveUserLockSelfServeBefore,
+							testUserLockSelfServe: &tc.testUserLockSelfServeBefore,
+							liveUserLockThreshold: &tc.liveUserLockThresholdBefore,
+							testUserLockThreshold: &tc.testUserLockThresholdBefore,
+						}),
+						Check: resource.ComposeAggregateTestCheckFunc(
+							resource.TestCheckResourceAttr("stytch_project.test", "name", "test"),
+							resource.TestCheckResourceAttr("stytch_project.test", "live_user_lock_self_serve_enabled", strconv.FormatBool(tc.liveUserLockSelfServeBefore)),
+							resource.TestCheckResourceAttr("stytch_project.test", "test_user_lock_self_serve_enabled", strconv.FormatBool(tc.testUserLockSelfServeBefore)),
+							resource.TestCheckResourceAttr("stytch_project.test", "live_user_lock_threshold", strconv.Itoa(int(tc.liveUserLockThresholdBefore))),
+							resource.TestCheckResourceAttr("stytch_project.test", "test_user_lock_threshold", strconv.Itoa(int(tc.testUserLockThresholdBefore))),
+						),
+					},
+					{
+						// Import state testing
+						ResourceName:      "stytch_project.test",
+						ImportState:       true,
+						ImportStateVerify: true,
+						// Ignore timestamp fields because rounding differences in various APIs can result in different
+						// values being returned
+						ImportStateVerifyIgnore: []string{"created_at", "last_updated"},
+					},
+					{
+						// Update and Read testing
+						Config: testutil.ProviderConfig + projectResource(projectResourceArgs{
+							name:                  "test2",
+							vertical:              projects.VerticalConsumer,
+							liveUserLockSelfServe: &tc.liveUserLockSelfServeAfter,
+							testUserLockSelfServe: &tc.testUserLockSelfServeAfter,
+							liveUserLockThreshold: &tc.liveUserLockThresholdAfter,
+							testUserLockThreshold: &tc.testUserLockThresholdAfter,
+						}),
+						Check: resource.ComposeAggregateTestCheckFunc(
+							resource.TestCheckResourceAttr("stytch_project.test", "name", "test2"),
+							resource.TestCheckResourceAttr("stytch_project.test", "live_user_lock_self_serve_enabled", strconv.FormatBool(tc.liveUserLockSelfServeAfter)),
+							resource.TestCheckResourceAttr("stytch_project.test", "test_user_lock_self_serve_enabled", strconv.FormatBool(tc.testUserLockSelfServeAfter)),
+							resource.TestCheckResourceAttr("stytch_project.test", "live_user_lock_threshold", strconv.Itoa(int(tc.liveUserLockThresholdAfter))),
+							resource.TestCheckResourceAttr("stytch_project.test", "test_user_lock_threshold", strconv.Itoa(int(tc.testUserLockThresholdAfter))),
+						),
+					},
+					// Delete testing automatically occurs in resource.TestCase
+				},
+			})
+		})
+	}
+
+	// Test user lock TTL settings
+	for _, tc := range []struct {
+		liveUserLockSelfServeBefore bool
+		testUserLockSelfServeBefore bool
+		liveUserLockSelfServeAfter  bool
+		testUserLockSelfServeAfter  bool
+		liveUserLockTTLBefore       int32
+		testUserLockTTLBefore       int32
+		liveUserLockTTLAfter        int32
+		testUserLockTTLAfter        int32
+	}{
+		{true, true, false, false, 300, 300, 1800, 1800},
+		{true, false, false, true, 300, 300, 1800, 1800},
+		{false, true, true, true, 300, 300, 1800, 1800},
+		// false, false is already tested above
+	} {
+		t.Run(fmt.Sprintf("user lock TTL %t-%t", tc.liveUserLockSelfServeBefore, tc.testUserLockSelfServeBefore), func(t *testing.T) {
+			resource.Test(t, resource.TestCase{
+				ProtoV6ProviderFactories: testutil.TestAccProtoV6ProviderFactories,
+				Steps: []resource.TestStep{
+					{
+						// Create and Read testing
+						Config: testutil.ProviderConfig + projectResource(projectResourceArgs{
+							name:                  "test",
+							vertical:              projects.VerticalConsumer,
+							liveUserLockSelfServe: &tc.liveUserLockSelfServeBefore,
+							testUserLockSelfServe: &tc.testUserLockSelfServeBefore,
+							liveUserLockTTL:       &tc.liveUserLockTTLBefore,
+							testUserLockTTL:       &tc.testUserLockTTLBefore,
+						}),
+						Check: resource.ComposeAggregateTestCheckFunc(
+							resource.TestCheckResourceAttr("stytch_project.test", "name", "test"),
+							resource.TestCheckResourceAttr("stytch_project.test", "live_user_lock_self_serve_enabled", strconv.FormatBool(tc.liveUserLockSelfServeBefore)),
+							resource.TestCheckResourceAttr("stytch_project.test", "test_user_lock_self_serve_enabled", strconv.FormatBool(tc.testUserLockSelfServeBefore)),
+							resource.TestCheckResourceAttr("stytch_project.test", "live_user_lock_ttl", strconv.Itoa(int(tc.liveUserLockTTLBefore))),
+							resource.TestCheckResourceAttr("stytch_project.test", "test_user_lock_ttl", strconv.Itoa(int(tc.testUserLockTTLBefore))),
+						),
+					},
+					{
+						// Import state testing
+						ResourceName:      "stytch_project.test",
+						ImportState:       true,
+						ImportStateVerify: true,
+						// Ignore timestamp fields because rounding differences in various APIs can result in different
+						// values being returned
+						ImportStateVerifyIgnore: []string{"created_at", "last_updated"},
+					},
+					{
+						// Update and Read testing
+						Config: testutil.ProviderConfig + projectResource(projectResourceArgs{
+							name:                  "test2",
+							vertical:              projects.VerticalConsumer,
+							liveUserLockSelfServe: &tc.liveUserLockSelfServeAfter,
+							testUserLockSelfServe: &tc.testUserLockSelfServeAfter,
+							liveUserLockTTL:       &tc.liveUserLockTTLAfter,
+							testUserLockTTL:       &tc.testUserLockTTLAfter,
+						}),
+						Check: resource.ComposeAggregateTestCheckFunc(
+							resource.TestCheckResourceAttr("stytch_project.test", "name", "test2"),
+							resource.TestCheckResourceAttr("stytch_project.test", "live_user_lock_self_serve_enabled", strconv.FormatBool(tc.liveUserLockSelfServeAfter)),
+							resource.TestCheckResourceAttr("stytch_project.test", "test_user_lock_self_serve_enabled", strconv.FormatBool(tc.testUserLockSelfServeAfter)),
+							resource.TestCheckResourceAttr("stytch_project.test", "live_user_lock_ttl", strconv.Itoa(int(tc.liveUserLockTTLAfter))),
+							resource.TestCheckResourceAttr("stytch_project.test", "test_user_lock_ttl", strconv.Itoa(int(tc.testUserLockTTLAfter))),
 						),
 					},
 					// Delete testing automatically occurs in resource.TestCase
