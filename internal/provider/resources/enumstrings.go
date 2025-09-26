f35197a9fdc37e1446208c1702f31fa8a7d5645f
mjiang-stytch <mjiang@stytch.com>
2025-09-25 21:48:41 -0700

    Remove all resources to start

--- internal/provider/resources/enumstrings.go
+++
@@ -1,9 +0,0 @@
-package resources
-
-func toStrings[T ~string](values []T) []string {
-	strs := make([]string, len(values))
-	for i, v := range values {
-		strs[i] = string(v)
-	}
-	return strs
-}
--- internal/provider/resources/ptr.go
+++
@@ -1,5 +0,0 @@
-package resources
-
-func ptr[T any](v T) *T {
-	return &v
-}
