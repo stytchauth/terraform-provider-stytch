0564640c9a0e0d73eaf931a0e65e2d0cd2a2cb26
Bruno Peynetti @ Stytch <bpeynetti@stytch.com>
2025-10-03 13:29:11 -0400

    [V3] Public tokens (#98)
    * Implement public tokens
    
    * Tweak docstring

---
+++ examples/resources/stytch_public_token/import.sh
@@ -0,0 +1,3 @@
+# A Stytch public token can be imported by specifying the project slug, environment slug, and public token value
+# Format: project_slug.environment_slug.public_token
+terraform import stytch_public_token.example my-project.production.public-token-test-81a0a2b4-9f4f-4c4e-971d-c99124c97e53
