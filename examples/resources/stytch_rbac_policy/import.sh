28e57f4b055fe2f3356783c3c085fd11e2717151
Bruno Peynetti @ Stytch <bpeynetti@stytch.com>
2025-10-10 09:35:47 -0400

    [V3] Implement RBAC policy resource (#109)
    * wip
    
    * Update RBAC policy resource to use default roles. Better docs and examples

--- examples/resources/stytch_rbac_policy/import.sh
+++ examples/resources/stytch_rbac_policy/import.sh
@@ -1,2 +1,2 @@
-# RBAC Policies can be imported by specifying the relevant project ID
-terraform import stytch_rbac_policy.example project-live-00000000-0000-0000-0000-000000000000
+# RBAC Policies can be imported by specifying the project slug and environment slug
+terraform import stytch_rbac_policy.example my-project-slug.my-environment-slug
--- examples/resources/stytch_rbac_policy/resource.tf
+++ examples/resources/stytch_rbac_policy/resource.tf
@@ -1,53 +1,54 @@
-# Manage a custom RBAC policy
-resource "stytch_rbac_policy" "b2b_rbac_policy" {
-  project_id = stytch_project.b2b_project.test_project_id
+# B2B Project RBAC Policy Example
+# This example shows how to configure RBAC for a B2B project with custom roles and resources
+
+resource "stytch_rbac_policy" "b2b_example" {
+  project_slug     = stytch_project.b2b_project.project_slug
+  environment_slug = stytch_environment.test.environment_slug
+
+  # Define custom roles
   custom_roles = [
     {
-      role_id     = "my-custom-admin"
-      description = "My custom admin role"
+      role_id     = "document-editor"
+      description = "Can create and edit documents"
       permissions = [
         {
-          resource_id = "my-resource"
-          actions     = ["create", "read", "update", "delete"]
-        },
-        {
-          resource_id = "my-other-resource"
-          actions     = ["read"]
+          resource_id = "custom-documents"
+          actions     = ["read", "create", "update"]
         }
       ]
     },
     {
-      role_id     = "my-custom-user"
-      description = "My custom user role"
+      role_id     = "document-viewer"
+      description = "Can only view documents"
       permissions = [
         {
-          resource_id = "my-resource"
-          actions     = ["read"]
-        },
-        {
-          resource_id = "my-other-resource"
+          resource_id = "custom-documents"
           actions     = ["read"]
         }
       ]
     }
   ]
+
+  # Define custom resources
   custom_resources = [
     {
-      resource_id       = "my-resource"
-      description       = "My custom resource"
+      resource_id       = "custom-documents"
+      description       = "Organization documents"
       available_actions = ["create", "read", "update", "delete"]
-    },
-    {
-      resource_id       = "my-other-resource"
-      description       = "My other custom resource"
-      available_actions = ["read"]
     }
   ]
 }
 
-# An example of adding a permission to the Stytch Admin role
-resource "stytch_rbac_policy" "b2b_rbac_policy_with_admin_perms" {
-  project_id = stytch_project.another_b2b_project.test_project_id
+# B2B Project with Modified Default Roles
+# This example shows how to add custom resource permissions to the stytch_admin default role
+# Note: You must include all required Stytch resource permissions when modifying default roles
+
+resource "stytch_rbac_policy" "b2b_with_admin_perms" {
+  project_slug     = stytch_project.b2b_project.project_slug
+  environment_slug = stytch_environment.test.environment_slug
+
+  # Configure the stytch_admin default role with additional permissions
+  # IMPORTANT: Must include all default Stytch permissions (stytch.member, stytch.organization, etc.)
   stytch_admin = {
     permissions = [
       {
@@ -67,39 +68,124 @@ resource "stytch_rbac_policy" "b2b_rbac_policy_with_admin_perms" {
         actions     = ["*"]
       },
       {
-        resource_id = "my-only-resource"
+        resource_id = "custom-documents"
+        actions     = ["*"]
+      }
+    ]
+  }
+
+  # Configure the stytch_member default role with limited permissions
+  stytch_member = {
+    permissions = [
+      {
+        resource_id = "stytch.member"
+        actions     = ["read"]
+      },
+      {
+        resource_id = "stytch.organization"
+        actions     = ["read"]
+      },
+      {
+        resource_id = "custom-documents"
         actions     = ["read"]
       }
     ]
   }
+
   custom_resources = [
     {
-      resource_id       = "my-only-resource"
-      description       = "My only resource"
-      available_actions = ["read", "write"]
+      resource_id       = "custom-documents"
+      description       = "Organization documents"
+      available_actions = ["create", "read", "update", "delete"]
     }
   ]
 }
 
-# INVALID! An example of trying to add to admin permissions without including the default Stytch permissions
-# This will fail during the tf apply stage of resource creation.
-# resource "stytch_rbac_policy" "I_AM_INVALID" {
-#   project_id = stytch_project.third_b2b_project.test_project_id
-#   stytch_admin = {
-#     permissions = [
-#       MISSING DEFAULT PERMISSIONS HERE!
-#       {
-#         resource_id = "my-only-resource"
-#         actions     = ["read"]
-#       }
-#     ]
-#   }
-# 
-#   custom_resources = [
-#     {
-#       resource_id       = "my-only-resource"
-#       description       = "My only resource"
-#       available_actions = ["read", "write"]
-#     }
-#   ]
-# }
+# Consumer Project RBAC Policy Example
+# This example shows how to configure RBAC for a Consumer project with custom scopes
+
+resource "stytch_rbac_policy" "consumer_example" {
+  project_slug     = stytch_project.consumer_project.project_slug
+  environment_slug = stytch_environment.test.environment_slug
+
+  # Configure the stytch_user default role (optional)
+  stytch_user = {
+    permissions = [
+      {
+        resource_id = "stytch.user"
+        actions     = ["read", "update"]
+      },
+      {
+        resource_id = "user-profile"
+        actions     = ["read", "update"]
+      }
+    ]
+  }
+
+  # Define custom roles
+  custom_roles = [
+    {
+      role_id     = "premium-user"
+      description = "Premium subscription user"
+      permissions = [
+        {
+          resource_id = "premium-content"
+          actions     = ["read", "download"]
+        }
+      ]
+    },
+    {
+      role_id     = "content-creator"
+      description = "Can create and manage content"
+      permissions = [
+        {
+          resource_id = "user-content"
+          actions     = ["create", "read", "update", "delete"]
+        }
+      ]
+    }
+  ]
+
+  # Define custom resources
+  custom_resources = [
+    {
+      resource_id       = "premium-content"
+      description       = "Premium subscription content"
+      available_actions = ["read", "download"]
+    },
+    {
+      resource_id       = "user-content"
+      description       = "User-generated content"
+      available_actions = ["create", "read", "update", "delete"]
+    },
+    {
+      resource_id       = "user-profile"
+      description       = "User profile information"
+      available_actions = ["read", "update"]
+    }
+  ]
+
+  # Define custom scopes (Consumer projects only)
+  custom_scopes = [
+    {
+      scope       = "premium:access"
+      description = "Access to premium features"
+      permissions = [
+        {
+          resource_id = "premium-content"
+          actions     = ["read"]
+        }
+      ]
+    },
+    {
+      scope       = "content:create"
+      description = "Ability to create content"
+      permissions = [
+        {
+          resource_id = "user-content"
+          actions     = ["create"]
+        }
+      ]
+    }
+  ]
+}
